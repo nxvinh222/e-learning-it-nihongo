fad8d9f238091d516254b80b3620bf8b4a5d34c9
trung-lha <62026316+trung-lha@users.noreply.github.com>
2021-06-14 03:00:25 +0700

    filter component

---
+++ src/components/Filter.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Dropdown } from "react-bootstrap";
+
+const Filter = (props) => {
+    const upper = (text) => {
+        return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+    return (
+        <>
+            <Dropdown>
+                <Dropdown.Toggle id="dropdown-button-dark-example1" variant="primary">
+                Filter by kind
+                </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                <Dropdown.Item 
+                     onClick={()=>{
+                        props.setFilter("");
+                    }}
+                    active
+                >All</Dropdown.Item>
+                {props.kinds.map((kind)=>{
+                    return(
+                        <Dropdown.Item onClick={()=>{
+                            props.setFilter(kind);
+                        }}>
+                            {upper(kind)}
+                        </Dropdown.Item>
+                        
+                    );
+                })}
+                
+                </Dropdown.Menu>
+            </Dropdown>
+        </>
+    );
+}
+
+export default Filter;
